ce7ed1f46affe375002b617fbd089bbaf38dd0ed
Viktorija <44357351+nikalav@users.noreply.github.com>
2019-02-21 11:29:45 +0100

    Add files via upload

--- code/script.js
+++ code/script.js
@@ -1,10 +1,14 @@
 //select DOM elements
 const template = document.querySelector("#set").content;
 const main = document.querySelector("main");
+const nav = document.querySelector("nav");
+const allLink = document.querySelector("#all");
 
 //make shortcuts to API endpoints
 const link = "https://spreadsheets.google.com/feeds/list/1RVK5ksJDyymK5QzZwrJ2KYxq5KKuAsBbbH0gmk9czps/1/public/values?alt=json";
 
+//add global eventListeners
+allLink.addEventListener("click", () => showCategory("all"));
 
 //define functions
 function loadJSON(link){
@@ -12,15 +16,29 @@ function loadJSON(link){
 
 }
 
+
 function displayLegoData(brickArt){
-	const section = document.querySelector('#' + brickArt.gsx$category.$t);
+	//const section = document.querySelector('#' + brickArt.gsx$category.$t);
+	let section = main.querySelector('#cat_'+brickArt.gsx$category.$t);
+	if(!section){
+		const newHeader = document.createElement("h1");
+		newHeader.textContent =brickArt.gsx$category.$t;
+		main.appendChild(newHeader);
+		section = document.createElement('section');
+		section.id='cat_'+brickArt.gsx$category.$t;
+		main.appendChild(section)
+		
+	}
 	let clone = template.cloneNode(true);
 	clone.querySelector("h2").textContent = brickArt.gsx$title.$t;
-	
+
 	const img = brickArt.gsx$imagename.$t;
 	clone.querySelector("img").setAttribute("src", "photos/"+img+".jpg");
-	
-	main.appendChild(clone);
-	
+	clone.querySelector("p").textContent = brickArt.gsx$description.$t;
+
+	section.appendChild(clone);
+
 }
+
+
 loadJSON(link);
