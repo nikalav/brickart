03dfe3fa71ec461f40a73205f7960d5774c0a16d
iuli0361@stud.kea.dk <iuli0361@stud.kea.dk>
2019-02-21 17:49:08 +0100

    navigation

--- code/script.js
+++ code/script.js
@@ -1,9 +1,17 @@
 //select DOM elements
 const template = document.querySelector("#set").content;
 const main = document.querySelector("main");
+const nav = document.querySelector("nav");
+const allLinks = document.querySelector("#all");
+
 
 //make shortcuts to API endpoints
 const link = "https://spreadsheets.google.com/feeds/list/1RVK5ksJDyymK5QzZwrJ2KYxq5KKuAsBbbH0gmk9czps/1/public/values?alt=json";
+
+
+//add EVENTLISTENERS
+
+allLinks.addEventListener("click", () => filter("all"));
 
 
 //define functions
@@ -13,14 +21,42 @@ function loadJSON(link){
 }
 
 function displayLegoData(brickArt){
-	const section = document.querySelector('#' + brickArt.gsx$category.$t);
+	let section = main.querySelector('#cat_'+brickArt.gsx$category.$t);
+	if(!section){
+        const newHeader = document.createElement("h1");
+		newHeader.textContent =brickArt.gsx$category.$t;
+		main.appendChild(newHeader)
+		section = document.createElement('section');
+		section.id='cat_'+brickArt.gsx$category.$t;
+		main.appendChild(section);
+        const a = document.createElement("a");
+        a.textContent = brickArt.gsx$category.$t;
+        a.href = "#";
+        a.addEventListener("click", ()=>filter(brickArt.gsx$category.$t));
+        nav.appendChild(a);
+
+
+	}
+
 	let clone = template.cloneNode(true);
 	clone.querySelector("h2").textContent = brickArt.gsx$title.$t;
-	
+
 	const img = brickArt.gsx$imagename.$t;
 	clone.querySelector("img").setAttribute("src", "photos/"+img+".jpg");
-	
-	main.appendChild(clone);
-	
+
+	section.appendChild(clone);
+
 }
+  function filter(category) {
+	document.querySelectorAll("main section").forEach(section => {
+		if (section.id == category || category == "all") {
+			section.style.display = "grid";
+			section.previousElementSibling.style.display = "block";
+		} else {
+			section.style.display = "none";
+			section.previousElementSibling.style.display = "none";
+		}
+	});
+}
+
 loadJSON(link);
